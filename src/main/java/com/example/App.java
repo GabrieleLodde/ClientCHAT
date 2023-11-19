fb78d3374f3de3a13054eb8a557e7c6aa76237f6
Gabriele Lodde <gabrilodde@gmail.com>
2023-11-19 23:56:51 +0100

    third commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,14 +5,14 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class App 
 {
-    public static void main( String[] args ) throws UnknownHostException, IOException
+    public static void main( String[] args )
     {
-        Socket s = new Socket("localhost", 4500);
         try {
+            Socket s = new Socket("localhost", 4500);
+
             BufferedReader inputTastiera = new BufferedReader(new InputStreamReader(System.in));
             BufferedReader inDalServer = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream outVersoIlServer = new DataOutputStream(s.getOutputStream());
@@ -26,33 +26,51 @@ public class App
             outVersoIlServer.writeBytes(stringaTastiera + "\n");
 
             while(!esci){
-                System.out.println("--MENU---");
+
+                // stringaRicevuta = inDalServer.readLine();
+                // if(!stringaRicevuta.equals("")){
+                //     System.out.println(stringaRicevuta);
+                // }
+
+                //questo mi blocca il client, perchè mi resta in attesa di un messaggio all'infinito
+
+                System.out.println("-----MENU-----");
                 System.out.println("> Invia un messaggio a tutti i partecipanti --> (1)");
                 System.out.println("> Invia un messaggio ad un partecipante specifico --> (2)");
                 System.out.println("> Abbandona il gruppo --> (3)");
+                System.out.println("--------------");
 
                 stringaTastiera = inputTastiera.readLine();
                 scelta = Integer.parseInt(stringaTastiera);
 
                 switch (scelta) {
-                    case 1:
+                    case 1:        //invio di un messaggio in broadcast
                         
+                        outVersoIlServer.writeBytes("1" + "\n");
+
+                        System.out.println("> Inserisci il messaggio da inviare...");
+                        stringaTastiera = inputTastiera.readLine();
+                        outVersoIlServer.writeBytes(stringaTastiera + "\n");
                         break;
 
-                    case 2:
+                    case 2:        //invio di un messaggio ad un partecipante specifico
 
+                        System.out.println("> Inserisci il nickname del partecipante con cui vuoi comunicare...");
+                        stringaTastiera = inputTastiera.readLine();
+                        //.............
                         break;
 
-                    case 3:
+                    case 3:        // termine della comunicazione 
+
                         esci = true;
                         outVersoIlServer.writeBytes("-1" + "\n");
                         System.out.println("Peccato, hai abbandonato il gruppo!");
                         break;
                 }
             }
+
             s.close();
         } catch (IOException e) {
-            e.getMessage();
             System.out.println("> Attenzione, errore nella creazione della comunicazione");
         }
             
