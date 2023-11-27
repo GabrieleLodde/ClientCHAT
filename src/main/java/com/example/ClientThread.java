7824855adcd0afe8d31a19fac46474b12e3435b2
lai2-24 <lai2-24@itismeucci.com>
2023-11-27 11:46:35 +0100

    sixth commit

--- src/main/java/com/example/ClientThread.java
+++ src/main/java/com/example/ClientThread.java
@@ -7,21 +7,23 @@ import java.net.Socket;
 
 public class ClientThread extends Thread{
     
-    Socket broadcastSocket;
-    BufferedReader inBroadcastDalServer;
-    boolean running;
-    String nome;
+    private Socket broadcastSocket;
+    private BufferedReader inBroadcastDalServer;
+    private boolean running;
+    private String nome;
+    private boolean entrato;
 
-    public ClientThread(Socket broadcastSocket, String nome) {
+    public ClientThread(Socket broadcastSocket, String nome, boolean entrato) {
         this.broadcastSocket = broadcastSocket;
         this.running = true;
         this.nome = nome;
         try {
-            this.inBroadcastDalServer = new BufferedReader(new InputStreamReader(broadcastSocket.getInputStream()));
+            this.inBroadcastDalServer = new BufferedReader(new InputStreamReader(this.broadcastSocket.getInputStream()));
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("> Errore nella creazione della comunicazione broadcast" + e.getMessage());
         }
+        this.entrato = entrato;
     }
 
     @Override
@@ -30,12 +32,14 @@ public class ClientThread extends Thread{
         while (this.running) {
             try {
                 messaggioRicevutoBroadcast = inBroadcastDalServer.readLine();
-                if(!messaggioRicevutoBroadcast.equals(null)){
-                    System.out.println(messaggioRicevutoBroadcast);
+                if(!messaggioRicevutoBroadcast.equals(null) && this.entrato == true ){
+                    if(checkNome(messaggioRicevutoBroadcast)){
+                        System.out.println(formattaMessaggio(messaggioRicevutoBroadcast) + "\n> Premere Invio");
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
-                System.out.println("> Errore nella comunicazione broadcast " + e.getMessage());
+                System.out.println("> Errore nella comunicazione broadcast");
             }
         }
         System.out.println("> Termine esecuzione thread client");
@@ -51,5 +55,22 @@ public class ClientThread extends Thread{
 
     public String getNome(){
         return this.nome;
+    }
+
+    public void setEntrato(){
+        this.entrato = true;
+    }
+
+    public boolean checkNome(String messaggio){
+        String[] array = messaggio.split(":");
+        if(array[1].equals(this.getNome())){
+            return false;
+        }
+        return true;
+    }
+
+    public String formattaMessaggio(String messaggio){
+        String[] array = messaggio.split(":");
+        return array[0] + array[1] + array[2] + array[3];
     }
 }
