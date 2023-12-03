6e27e87d2f8ef97911e47f606c81ca690ec43a55
Gabriele Lodde <gabrilodde@gmail.com>
2023-12-03 18:55:21 +0100

    final commit client side

--- src/main/java/com/example/ClientThread.java
+++ src/main/java/com/example/ClientThread.java
@@ -54,7 +54,7 @@ public class ClientThread extends Thread {
         this.running = running;
     }
 
-    // Method for formatting and printing messages received from the server based on
+    // Method for formatting and checking messages received from the server based on
     // their syntactic nature
     public void formatMessage(String messageReceived) {
         // Creation of the string array coming from the string received from the server
@@ -62,16 +62,12 @@ public class ClientThread extends Thread {
         // Check if the server sent the nickname of a new client that initiated the
         // communication
         if (arrayString[0].equals("@nick")) {
-            System.out.println(color.PURPLE_BOLD_BRIGHT + "> " + color.RESET + color.BLUE_BOLD_BRIGHT + arrayString[1]
-                    + color.RESET + color.CYAN_BOLD_BRIGHT + " si e' unito/a al gruppo! " + color.RESET
-                    + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now) + color.RESET + "\n");
+            printConfirmationMessage(color.BLUE_BOLD_BRIGHT, arrayString[1], color.CYAN_BOLD_BRIGHT,
+                    " si e' unito/a al gruppo! ");
         }
         // Check if the server has forwarded a message previously broadcast by a client
         else if (arrayString[0].equals("@all")) {
-            System.out.println(color.PURPLE_BOLD_BRIGHT + "> " + color.RESET + color.BLUE_BOLD_BRIGHT + arrayString[1]
-                    + color.RESET + color.WHITE_BOLD_BRIGHT + " ha scritto > " + color.RESET + color.YELLOW_BOLD_BRIGHT
-                    + "'" + arrayString[2] + "' " + color.RESET + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now)
-                    + color.RESET + "\n");
+            printTextMessage(color.BLUE_BOLD_BRIGHT, arrayString[1], color.WHITE_BOLD_BRIGHT, " ha scritto > ", color.YELLOW_BOLD_BRIGHT, ("'" + arrayString[2] + "' "));
         }
         // Check if the server has forwarded the list of nicknames of connected clients,
         // previously requested by a client
@@ -82,71 +78,82 @@ public class ClientThread extends Thread {
         // Check if the server forwarded the nickname of the client that interrupted the
         // communication
         else if (arrayString[0].equals("@exit")) {
-            System.out.println(color.PURPLE_BOLD_BRIGHT + "> " + color.RESET + color.BLUE_BOLD_BRIGHT + arrayString[1]
-                    + color.RESET + color.CYAN_BOLD_BRIGHT + " ha abbandonato il gruppo! " + color.RESET
-                    + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now) + color.RESET + "\n");
+            printConfirmationMessage(color.BLUE_BOLD_BRIGHT, arrayString[1], color.CYAN_BOLD_BRIGHT, " ha abbandonato il gruppo! ");
         }
         // Check if the server forwarded an error sending a broadcast message because
         // only one client is connected
         else if (arrayString[0].equals("@alone1")) {
-            System.out.println(
-                    color.RED_BOLD_BRIGHT + "Impossibile inviare il messaggio in broadcast, sei presente solo tu! "
-                            + color.RESET + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now) + color.RESET + "\n");
+            printAloneMessage(color.RED_BOLD_BRIGHT,
+                    "Impossibile inviare il messaggio in broadcast, sei presente solo tu! ");
         }
         // Check if the server forwarded an error sending a private message, because
         // only one client is connected
         else if (arrayString[0].equals("@alone2")) {
-            System.out
-                    .println(color.RED_BOLD_BRIGHT + "Impossibile inviare il messaggio privato, sei presente solo tu! "
-                            + color.RESET + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now) + color.RESET + "\n");
+            printAloneMessage(color.RED_BOLD_BRIGHT,
+                    "Impossibile inviare il messaggio privato, sei presente solo tu! ");
         }
         // Check if the server forwarded an error in sending a private message, because
         // the nickname of the searched client is not present at the moment
         else if (arrayString[0].equals("@wrong")) {
-            System.out.println(color.RED_BOLD_BRIGHT + "Impossibile inviare il messaggio privato: " + color.RESET
-                    + color.BLUE_BOLD_BRIGHT + arrayString[1] + color.RESET + color.RED_BOLD_BRIGHT
-                    + " non fa parte del gruppo! " + color.RESET + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now)
-                    + color.RESET + "\n");
+            printErrorNickname(color.RED_BOLD_BRIGHT, "Impossibile inviare il messaggio privato: ",
+                    color.BLUE_BOLD_BRIGHT, arrayString[1], " non fa parte del gruppo! ");
         }
         // Check if the server forwarded an error in sending a private message,
         // because the nickname of the client being searched for is the same as the one
         // who requested to send the same message
         else if (arrayString[0].equals("@you")) {
-            System.out.println(color.RED_BOLD_BRIGHT + "Impossibile inviare il messaggio privato: " + color.RESET
-                    + color.BLUE_BOLD_BRIGHT + arrayString[1] + color.RESET + color.RED_BOLD_BRIGHT
-                    + " sei tu stesso/a!" + color.RESET + "\n");
+            printErrorNickname(color.RED_BOLD_BRIGHT, "Impossibile inviare il messaggio privato: ",
+                    color.BLUE_BOLD_BRIGHT, arrayString[1], " sei tu stesso/a! ");
         }
         // Check if the server has forwarded a private message to a specific client
         else if (arrayString[0].equals("@only")) {
-            System.out.println(color.PURPLE_BOLD_BRIGHT + "> " + color.RESET + color.BLUE_BOLD_BRIGHT + arrayString[1]
-                    + color.RESET + color.WHITE_BOLD_BRIGHT + " ti ha scritto > " + color.RESET
-                    + color.YELLOW_BOLD_BRIGHT + "'" + arrayString[2] + "' " + color.RESET
-                    + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now) + color.RESET + "\n");
+            printTextMessage(color.BLUE_BOLD_BRIGHT, arrayString[1], color.WHITE_BOLD_BRIGHT, " ti ha scritto > ",
+                    color.YELLOW_BOLD_BRIGHT, ("'" + arrayString[2] + "' "));
         }
         // Check if the server has forwarded a confirmation message to a client
         // regarding the previous sending of a broadcast message
         else if (arrayString[0].equals("@ok1")) {
-            printConfirmationBroadcastMessage(color.GREEN_BOLD_BRIGHT, "Messaggio inviato in broadcast!");
+            printConfirmationBroadcastMessage(color.GREEN_BOLD_BRIGHT, "Messaggio inviato in broadcast! ");
         }
         // Check if the server has forwarded a confirmation message to a client
         // regarding the previous sending of a message privately
         else if (arrayString[0].equals("@ok2")) {
-            printConfirmationPrivateMessage(color.GREEN_BOLD_BRIGHT, "Messaggio inviato in privato a ",
+            printConfirmationMessage(color.GREEN_BOLD_BRIGHT, "Messaggio inviato in privato a ",
                     color.BLUE_BOLD_BRIGHT, (arrayString[1] + "! "));
         }
     }
 
+    // Various methods of printing messages
     public void printConfirmationBroadcastMessage(String firstColor, String firstString) {
         System.out.println(color.PURPLE_BOLD_BRIGHT + "> " + color.RESET + firstColor + firstString + color.RESET
                 + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now)
                 + color.RESET + "\n");
     }
 
-    public void printConfirmationPrivateMessage(String firstColor, String firstString, String secondColor,
+    public void printConfirmationMessage(String firstColor, String firstString, String secondColor,
             String secondString) {
         System.out.println(color.PURPLE_BOLD_BRIGHT + "> " + color.RESET + firstColor + firstString + color.RESET
                 + secondColor + secondString + color.RESET + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now)
                 + color.RESET + "\n");
     }
 
+    public void printTextMessage(String firstColor, String firstString, String secondColor, String secondString,
+            String thirdColor, String thirdString) {
+        System.out.println(color.PURPLE_BOLD_BRIGHT + "> " + color.RESET + firstColor + firstString + color.RESET
+                + secondColor + secondString + color.RESET + thirdColor + thirdString + color.RESET
+                + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now) + color.RESET + "\n");
+    }
+
+    public void printErrorNickname(String firstColor, String firstString, String secondColor, String secondString,
+            String thirdString) {
+        System.out.println(firstColor + firstString + color.RESET
+                + secondColor + secondString + color.RESET + firstColor + thirdString + color.RESET
+                + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now)
+                + color.RESET + "\n");
+    }
+
+    public void printAloneMessage(String firstColor, String firstString) {
+        System.out.println(firstColor + firstString + color.RESET + color.BLACK_BACKGROUND_BRIGHT + dtf.format(now)
+                + color.RESET + "\n");
+    }
 }
